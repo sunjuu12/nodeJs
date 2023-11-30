fc7a522056ef94fabde98accc9c8124590653d6f
sunjuu12 <haeju7368@naver.com>
2023-11-30 17:52:00 +0900

    1130 vue,js

---
+++ vue/component-project/src/views/DataBinding.vue
@@ -0,0 +1,45 @@
+<template>
+   <div>
+        <h1>{{ title + ' !!!' }}</h1>
+        <h2 v-bind:name="'sample'" v-text="title" />
+        <p v-html="tagList" />
+        <p v-text="tagList" />
+        <hr>
+        <input type="text" v-model="valueModel">
+        <p>{{ typeof valueModel }}</p>
+        <input type="number" v-model.lazy="numberModel">
+        <p>{{ numberModel + 10000 }}</p>
+        <select v-model="selectModel">
+            <option value="spring">봄</option>
+            <option value="summer">여름</option>
+            <option value="winter">겨울</option>
+        </select>
+        <p>{{ selectModel }}</p>
+        <textarea v-model="textModel" />
+        <hr>
+        <input type="checkbox" v-model="chData" true-value="여" false-value="부">
+        <p>{{ chData }}</p>
+        <div>
+            <lavel><input type="checkbox" value="서울" v-model="city">서울</lavel>
+            <lavel><input type="checkbox" value="대구" v-model="city">대구</lavel>
+            <lavel><input type="checkbox" value="부산" v-model="city">부산</lavel>
+            <p>{{ city }}</p>
+        </div>
+    </div> 
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title : 'Hello, Vue.js',
+            tagList : '<strong> Today is ... </strong>',
+            valueModel : 'Korea',
+            numberModel : 0,
+            selectModel : 'winter',
+            textModel : '백견불여일타',
+            chData : '',
+            city : ''
+        }
+    }  
+}
+</script> 
---
+++ vue/vue-test/src/components/VueComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <label><input type="text" v-model="msg"></label>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+<script>
+    export default {
+        name : 'VueComponent',
+        data() {
+            return {
+                msg : '2023-11-30'
+            }
+        }
+    }
+</script>
+<style scoped>
+    p {
+        background-color: burlywood;
+    }
+</style>
